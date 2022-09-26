a181e9a8ab23c6fcd47bb479bfc27364b4f73663
DannaTorres22 <dltorres862@misena.edu.co>
2022-09-26 16:45:15 -0500

    rutas paginas

--- reacthotelia/src/index.js
+++ reacthotelia/src/index.js
@@ -1,49 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Navigate,Route,Routes} from 'react-router-dom';
+import Inicio from './pages/inicio/Inicio.js'
+import Login from './pages/login/Login.js';
+import Registrar from './pages/Registrar/Registrar.js';
 import './index.css';
 
 
 
-import Login from './components/login/login';
-
-/*
-import Login from './components/login/login';
- <Login/>
-*/ 
-
-/*    
-import Navbar from './components/navbar/navbar';
-//import Banner from './components/banner/banner';     <Banner/>
-import Info from './components/info/info';
-import Feat from './components/features/feat';
-import Ubicanos from './components/ubicanos/ubi';
-import Opinion from './components/opinion/opi';
-import Footer from './components/footer/footer';
-    <Navbar/>
-    
-    <Info/>
-    <Feat/>
-    
-    <Ubicanos/>
-    <Opinion/>
-    <Footer/> 
-
-*/
-
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Routes>
+    <Route path='/' element={<Inicio/>}/>
+    <Route path='/' element={<Login/>}/>
+    <Route path='/' element={<Registrar/>}/>
  
- <Login/>
-
+    <Route path='*' element={<Error404/>}/>
+    </Routes>
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
 
